909effe7f13a24e3d8740e97f409302afacb9c2f
artem <artem2006stepanov@yandex.ru>
2025-12-16 22:14:21 +0300

    statistic handler

--- API-Gateway/src/api/__init__.py
+++ API-Gateway/src/api/__init__.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter
 
 from src.api.users import router as users_router
 from src.api.ai import router as ai_router
+from src.api.statistic import router as stat_router
 
 router = APIRouter()
 
 router.include_router(users_router)
 router.include_router(ai_router)
+router.include_router(stat_router)
